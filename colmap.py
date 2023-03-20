5778241837a6678906f911828d7f68c4e0a95d3e
yc4ny <yhugestar@snu.ac.kr>
2023-03-21 01:10:09 +0900

    Added running colmap on undistorted images

--- colmap.py
+++ colmap.py
@@ -1,41 +1,80 @@
-import os 
+import os
+import argparse
 
-def main():
-    # Create Database 
-    cmd = "colmap database_create \
-    --database_path colmap_data/database.db"
+def create_database(database_path):
+    cmd = f"colmap database_create --database_path {database_path}"
     os.system(cmd)
+
+def feature_extraction(database_path, image_path, single_camera, camera_model):
+    cmd = f"colmap feature_extractor \
+            --database_path {database_path} \
+            --image_path {image_path} \
+            --ImageReader.single_camera {single_camera} \
+            --ImageReader.camera_model {camera_model}"
+    os.system(cmd)
+
+def feature_matching(database_path):
+    cmd = f"colmap exhaustive_matcher --database_path {database_path}"
+    os.system(cmd)
+
+def sparse_reconstruction(database_path, image_path, output_path):
+    os.makedirs(output_path, exist_ok=True)
+    cmd = f"colmap mapper \
+            --database_path {database_path} \
+            --image_path {image_path} \
+            --output_path {output_path}"
+    os.system(cmd)
+
+def save_output_as_txt(input_path, output_path, output_type):
+    os.makedirs(output_path, exist_ok=True)
+    cmd = f"colmap model_converter \
+            --input_path {input_path} \
+            --output_path {output_path} \
+            --output_type {output_type}"
+    os.system(cmd)
+
+def main(args):
+    # Initial or undistorted images
+    image_path = "preprocessed/sampled_scene" if args.initial else "preprocessed/undistorted_sampled_scene"
+    data_folder = "colmap_data" if args.initial else "colmap_data_undistort"
+
+    os.makedirs(data_folder, exist_ok=True)
+
+    # Create Database
+    create_database(database_path=f"{data_folder}/database.db")
 
     # Feature Extraction
-    cmd = "colmap feature_extractor \
-    --database_path colmap_data/database.db \
-    --image_path preprocessed/sampled_scene \
-    --ImageReader.single_camera 1 \
-    --ImageReader.camera_model OPENCV"
-    os.system(cmd)
+    feature_extraction(
+        database_path=f"{data_folder}/database.db",
+        image_path=image_path,
+        single_camera=1,
+        camera_model="OPENCV"
+    )
 
     # Feature Matching
-    cmd = "colmap exhaustive_matcher \
-    --database_path colmap_data/database.db"
-    os.system(cmd)
+    feature_matching(database_path=f"{data_folder}/database.db")
 
     # Sparse Reconstruction
-    cmd = "mkdir colmap_data/sparse"
-    os.system(cmd)
-    cmd = "colmap mapper \
-    --database_path colmap_data/database.db \
-    --image_path preprocessed/sampled_scene \
-    --output_path colmap_data/sparse"
-    os.system(cmd)
+    sparse_reconstruction(
+        database_path=f"{data_folder}/database.db",
+        image_path=image_path,
+        output_path=f"{data_folder}/sparse"
+    )
 
     # Save output as txt file
-    cmd = "mkdir colmap_data/txt"
-    os.system(cmd)
-    cmd = "colmap model_converter \
-    --input_path colmap_data/sparse/0 \
-    --output_path colmap_data/txt \
-    --output_type TXT"
-    os.system(cmd)
+    save_output_as_txt(
+        input_path=f"{data_folder}/sparse/0",
+        output_path=f"{data_folder}/txt",
+        output_type="TXT"
+    )
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Run COLMAP with initial or undistorted images.")
+    parser.add_argument("--initial", action="store_true", help="Use initial images (default)")
+    parser.add_argument("--undistorted", action="store_true", help="Use undistorted images")
+    args = parser.parse_args()
+
+    if not (args.initial or args.undistorted):
+        args.initial = True
+
+    main(args)
