5c58250a4e29d2144b738a58ed6309223b70f04e
yc4ny <yhugestar@snu.ac.kr>
2023-04-26 00:19:38 +0900

    Added code for renaming images in order

---
+++ utils/rename_inorder.py
@@ -0,0 +1,38 @@
+import os  
+import re  
+import argparse 
+
+# Define command-line arguments
+parser = argparse.ArgumentParser(description="Rename image files in a folder")
+parser.add_argument("--folder_path", help="Path to the folder containing image files")
+parser.add_argument("--base_name", help="Base name for the new file names", default="image")
+args = parser.parse_args()
+
+# Define the main function for renaming files
+def rename_files_in_folder(folder_path, base_name):
+    # List all files in the folder
+    files = os.listdir(folder_path)
+
+    # Filter image files with the pattern: base_name_XXXXX.jpg
+    pattern = re.compile(f"{base_name}_\d{{5}}\.jpg")
+    image_files = [f for f in files if pattern.match(f)]
+
+    # Sort image files
+    image_files.sort()
+
+    # Iterate over the image files and rename them
+    for index, image_file in enumerate(image_files, start=1):
+        old_path = os.path.join(folder_path, image_file)
+        new_name = f"{base_name}_{index:05d}.jpg"
+        new_path = os.path.join(folder_path, new_name)
+        os.rename(old_path, new_path)
+        print(f"Renamed {old_path} to {new_path}")
+
+# Call the main function with the command-line arguments
+if __name__ == "__main__":
+    # Parse command-line arguments
+    folder_path = args.folder_path
+    base_name = args.base_name
+
+    # Call the function to rename files
+    rename_files_in_folder(folder_path, base_name)
