97a87224136f11d10b020262a5e99628b9735e78
yc4ny <yhugestar@snu.ac.kr>
2023-04-26 00:16:50 +0900

    Added code for concating images in two different folders sequentially

--- utils/concat_img.py
+++ utils/concat_img.py
@@ -1,19 +1,22 @@
-import os
-import argparse
-import cv2
-from tqdm import tqdm 
+import os  
+import argparse  
+import cv2  
+from tqdm import tqdm  
 
+# Define command-line arguments
 parser = argparse.ArgumentParser(description="Concatenate images vertically")
 parser.add_argument("--f1", help="Path to the first folder containing images")
 parser.add_argument("--f2", help="Path to the second folder containing images")
-parser.add_argument("--output", help="Path to the output folder", default="output")
+parser.add_argument("--output", help="Path to the output folder")
 args = parser.parse_args()
 
+# Define the main function for concatenating images
 def concatenate_images(folder1, folder2, output_folder):
     # Get the sorted image files from both folders
     images_folder1 = sorted([f for f in os.listdir(folder1) if f.endswith(('.jpg', '.jpeg', '.png'))])
     images_folder2 = sorted([f for f in os.listdir(folder2) if f.endswith(('.jpg', '.jpeg', '.png'))])
 
+    # Ensure that both folders have the same number of images
     if len(images_folder1) != len(images_folder2):
         raise ValueError("The number of images in both folders must be the same")
 
@@ -26,15 +29,17 @@ def concatenate_images(folder1, folder2, output_folder):
         img_path1 = os.path.join(folder1, img_file1)
         img_path2 = os.path.join(folder2, img_file2)
 
+        # Read the images and ensure they have the same width
         img1 = cv2.imread(img_path1)
         img2 = cv2.imread(img_path2)
-
         if img1.shape[1] != img2.shape[1]:
             raise ValueError(f"Images {img_file1} and {img_file2} must have the same width")
 
-        concatenated_img = cv2.hconcat([img1, img2])
+        # Concatenate the images vertically and save the output
+        concatenated_img = cv2.vconcat([img1, img2])
         output_image_path = os.path.join(output_folder, img_file1)
         cv2.imwrite(output_image_path, concatenated_img)
 
+# Call the main function with the command-line arguments
 if __name__ == "__main__":
     concatenate_images(args.f1, args.f2, args.output)
