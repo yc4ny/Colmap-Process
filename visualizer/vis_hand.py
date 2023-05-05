3e72b589de732174c29345d52066efdc121c5a3f
yc4ny <yhugestar@snu.ac.kr>
2023-05-05 16:22:32 +0900

    Refined visualization code

--- visualizer/vis_hand.py
+++ visualizer/vis_hand.py
@@ -7,6 +7,7 @@ import time
 import json 
 import argparse 
 
+# Function to align the 3D joints to the camera center
 def align_joints_to_camera(joints, camera_location):
     # Calculate the translation vector needed to align the first joint with the camera location
     translation_vector = camera_location - joints[0]
@@ -20,20 +21,21 @@ def align_joints_to_camera(joints, camera_location):
 
     return aligned_joints
 
+# Function to create joints and connections in 3D space 
 def create_hand_geometry(joints, connections, color=[1, 0, 0]):
     hand_pcd = o3d.geometry.PointCloud()
     hand_pcd.points = o3d.utility.Vector3dVector(joints)
-    hand_pcd.paint_uniform_color(color)
+    hand_pcd.paint_uniform_color(color) # Red color for joint locations
     hand_pcd.estimate_normals()
 
     hand_lines = o3d.geometry.LineSet()
     hand_lines.points = hand_pcd.points
     hand_lines.lines = o3d.utility.Vector2iVector(connections)
-    hand_lines.colors = o3d.utility.Vector3dVector([[0, 0, 1] for _ in range(len(connections))])  # Blue color for the lines
+    hand_lines.colors = o3d.utility.Vector3dVector([[0, 0, 1] for _ in range(len(connections))])  # Blue color for the lines/connections
 
     return hand_pcd, hand_lines
 
-
+# Function to extract camera extrinsics, 3D joints from input files and align, create geometries of the final geometries which needs to be rendered
 def prepare_frame_geometry(data, connections, extrinsics, scale=5):
     frame_geometry = []
     base_name = os.path.splitext(os.path.basename(data['image_path']))[0]
@@ -77,7 +79,7 @@ def load_view(json_path):
     return trajectory['field_of_view'], trajectory['front'], trajectory['lookat'], trajectory['up'], trajectory['zoom']
 
 
-def visualize_3d_points(pkl_files, connections, ply_file_path, scale=10, extrinsics=None, capture = None):
+def visualize_3d_points(pkl_files, connections, ply_file_path, scale=10, extrinsics=None, capture = None, output = None):
     # Load the PLY file
     colmap_pcd = o3d.io.read_point_cloud(ply_file_path)
     colmap_pcd.paint_uniform_color([0.5, 0.5, 0.5])  # Grey color for the points from the PLY file
@@ -89,26 +91,42 @@ def visualize_3d_points(pkl_files, connections, ply_file_path, scale=10, extrins
     field_of_view, front, lookat, up, zoom = load_view(f"data/views/view_{capture}.json")
     ctr = vis.get_view_control()
 
+    # Initialize a counter for saving frames of visualized hand joints
+    counter = 0
     # Iterate through all the .pkl files, load the joint data, and visualize the hand movements sequentially
     for pkl_file in pkl_files:
         with open(pkl_file, 'rb') as f:
             data = pickle.load(f)
         # Prepare and visualize the scene for the current frame
         print(pkl_file)
+
+        # Add all the necessary geometries in the frame
         frame_geometry = prepare_frame_geometry(data, connections, extrinsics, scale)
         for hand_geom in frame_geometry:
             for geom in hand_geom:
                 vis.add_geometry(geom)
+        
+        # Change field of view obtained from get_view function 
         ctr.change_field_of_view(field_of_view)
         ctr.set_front(front)
         ctr.set_lookat(lookat)
         ctr.set_up(up)
         ctr.set_zoom(zoom)
+
+        # Update Renderer
         vis.poll_events()
         vis.update_renderer()
+
+        # Set framerate: 1/30 = 30fps
         time.sleep(1/30)
 
-        # Remove current frame geometries before adding new ones
+        # Save the image for current frame
+        vis.capture_screen_image(f"visualizer/vis_img/{counter:05}.jpg")
+        
+        # Increment the counter
+        counter += 1
+
+        # Remove current geometries before adding new ones
         for hand_geom in frame_geometry:
             for geom in hand_geom:
                 vis.remove_geometry(geom)
@@ -116,29 +134,34 @@ def visualize_3d_points(pkl_files, connections, ply_file_path, scale=10, extrins
     vis.destroy_window()
 
 def main():
-    parser = argparse.ArgumentParser(description='Visualize 3D points from capture.')
-    parser.add_argument('--capture', type=str, required=True, help='Name of the capture to visualize.', default = "desk")
+
+    parser = argparse.ArgumentParser(description='Visualize 3D Hand in 3D Space.')
+    parser.add_argument('--capture', type=str, required=True, help='Name of the captured data.', default = "desk")
+    parser.add_argument('--output', type=str, required=True, help='Save directory.', default = "visualizer/vis_img")
     args = parser.parse_args()
 
+    # Hand joint connections
+    # https://github.com/facebookresearch/frankmocap/blob/main/docs/joint_order.md
     connections = [
         [0, 1],[1, 2],[2, 3],[3, 4],[0, 5],[5, 6],[6, 7],[7, 8],[0, 9],[9, 10],[10, 11],[11, 12],
         [0, 13],[13, 14],[14, 15],[15, 16],[0, 17],[17, 18],[18, 19],[19, 20]]
 
+    # Load scene pointcloud
     ply_file_path = f'data/{args.capture}/colmap_data/sparse/0/points.ply'
+    # Load 3d hand keypoint outputs from frankmocap's hand mocap detector
     pkl_files = sorted(glob.glob(f'data/{args.capture}/frankmocap_joints/*.pkl'))
 
+    #Load the saved extrinsic parameters from colmap's register-image (EPNP) (Head, Left Wrist, Right Wrist Cameras)
     with open(f'data/{args.capture}/camera_extrinsic/head_extrinsic.pkl', 'rb') as f:
         head_extrinsics = pickle.load(f)
-
     with open(f'data/{args.capture}/camera_extrinsic/left_extrinsic.pkl', 'rb') as f:
         left_extrinsics = pickle.load(f)
-
     with open(f'data/{args.capture}/camera_extrinsic/right_extrinsic.pkl', 'rb') as f:
         right_extrinsics = pickle.load(f)
-
     extrinsics = {'head': head_extrinsics, 'left': left_extrinsics, 'right': right_extrinsics}
 
-    visualize_3d_points(pkl_files, connections, ply_file_path, scale=5, extrinsics=extrinsics, capture = args.capture)
+    # Run the Open3D visualizer for visualizing 3D hand in the reconstructed 3D scene from colmap
+    visualize_3d_points(pkl_files, connections, ply_file_path, scale=5, extrinsics=extrinsics, capture = args.capture, output = args.output)
 
 if __name__ == "__main__":
     main()
